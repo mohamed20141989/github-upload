7c8754a4da214e32210eb75ecb5d63e9ac2ee970
mohamed20141989 <70681702+mohamed20141989@users.noreply.github.com>
2020-09-03 12:58:56 +0530

    test

---
+++ BankController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wholesale.Models;
+
+namespace Wholesale.Controllers
+{
+    public class BankController : Controller
+    {
+        private  bankDetailsEntities db = new bankDetailsEntities();
+        // GET: Bank
+        public ActionResult Index()
+        {
+            return View(db.AccountLists.ToList());
+        }
+        public ActionResult Transaction(string Id)
+        {
+            return View(db.AccountTransactions.Where(x=>x.AccountNumber==Id).ToList());
+        }
+
+    }
+}
